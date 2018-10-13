a808258ac9d31aa1c93e8dd2379880c56fae5866
estelle <stellaou13@gmail.com>
2018-10-13 10:26:12 +0200

    Diagnostic form WIP

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -9,6 +9,12 @@ class QuestionnaireView(FormView):
     form_class = QuestionsForm
     template_name = "questionnaire.html"
     success_url = '/merci/'
+    section_title = "BIEN-ÊTRE ET ENVIRONNEMENT"
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["section_title"] = self.section_title
+        return context
